58415de2dc591dd179a489ccc6253aa03a0ad20b
Matthew Shan <matthewshan99@gmail.com>
2025-08-19 21:47:27 -0400

    WIP

---
+++ api/MinecraftApi.Rcon/Adapters/RconAdapter.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace MinecraftApi.Rcon.Adapters;
+public class RconAdapter(IMemoryCache memoryCache)
+{
+        // mem cache
+}
--- api/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/matthewshan/minecraft-api
-
-go 1.24.6
