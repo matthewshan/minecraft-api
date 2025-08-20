1fb713024c379ce07b151542fa14bff425a53f27
Matthew Shan <matthewshan99@gmail.com>
2025-08-20 13:56:16 -0400

    Deletes open telemetry from next js application

--- ui/src/app/api/mc/list/route.ts
+++ ui/src/app/api/mc/list/route.ts
@@ -1,28 +1,21 @@
 import { PlayerListResponse } from '@/app/models/player';
 import axios from 'axios';
 import { NextResponse } from 'next/server';
-import { LoggerProvider } from '@opentelemetry/sdk-logs';
-
-const loggerProvider = new LoggerProvider();
-const logger = loggerProvider.getLogger('nextjs-logger');
 
 const MINECRAFT_API_URL = process.env.MINECRAFT_API_URL;
 
 export async function GET() {
     try {
-        logger.emit({
-            severityText: 'INFO',
-            body: 'Listing players from Minecraft API. Uri: \'{ui}\'',
-            attributes: {
-                uri: `${MINECRAFT_API_URL}/api/players`
-            }
-        });
         const uri = `${MINECRAFT_API_URL}/api/players`;
+        if (!MINECRAFT_API_URL) {
+            throw new Error("MINECRAFT_API_URL is not defined in environment variables");
+        }
+
         console.log(`Listing players from Minecraft API: '${uri}'`);
         const response = await axios.get(`${MINECRAFT_API_URL}/api/players`);
         return NextResponse.json<PlayerListResponse>(response.data);
     } catch (error) {
         console.error("Error listing players:", error);
-        return new NextResponse("Failed to list players", { status: 500 });
+        return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
